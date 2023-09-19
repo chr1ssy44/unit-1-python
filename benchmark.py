b77baf110158d15529766e13dae7eac92d78df9a
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2023-09-19 09:50:33 -0400

    added a file

---
+++ benchmark.py
@@ -0,0 +1,35 @@
+# exercise 1:
+print('Christina')
+
+#exercise 2:
+favorite_show= 'The Parkers'
+
+#exercise 3:
+height= 62
+age= 16
+
+print(height * age)
+
+#exercise 4:
+print('im going to break')
+
+# exercise 5:
+fav_food=['pizza', 'chicken', 'tacos']
+
+#exercise 6:
+if age >= 21:
+    print('you can rent a car')
+else:
+    print('your beat')
+
+#exercise 7:
+name= input
+input('what is your name?')
+print('Hello' + name)
+
+#exercise 8:
+
+#exercise 9:
+
+
+
