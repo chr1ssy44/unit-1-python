c6ebee33c346385982a668db4a4a06387452133f
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2023-09-29 10:14:26 -0400

    added a file

---
+++ whimsical_whiles.py
@@ -0,0 +1,55 @@
+"""
+1. Simple Counter:
+Write a program that uses a while loop to print numbers from 1 to 10.
+"""
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
+# I made x = 1 and made a while loop to make x less than or equal to 10 since we want it to print from 1 to 10. Then I added 1 everytime to x until it got to 10.
+"""
+2. Countdown:
+Write a program that uses a while loop to print numbers from 10 to 1 in descending order.
+"""
+y = 10
+while y >= 1 :
+    print(y)
+    y -= 1
+# I made y = 10 and made a while loop y is greater than or equal to 1, then print y and subtract by 1 to descend.
+"""
+3. Factorial Calculation:
+Write a program that calculates the factorial of a given number using a while loop.
+"""
+
+"""
+4. Password Guessing Game:
+Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
+"""
+z = input("Guess the password: ")
+password = ("password")
+
+while z == password:
+    print("You are correct!")
+    if z == password:
+        break
+else:
+    print("You are wrong. Try again!")
+# I made to variable one for the input and one for the password. Then I made z equal to password and if the user got the password right the while loop would break which means it would end. I fthey got it wrong they would get a message to try again.
+"""
+5. Sum of Digits:
+Write a program that calculates the sum of the digits of a given number using a while loop.
+"""
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+while a & b >= 0:
+    print(a + b)
+    break
+# I created two variables for the users input then created a while loop the make a and b greater than or equal to zero. Then to print the sum of the two digits.
+"""
+6. Fibonacci Series:
+Write a program that prints the first n numbers in the Fibonacci sequence using a while loop.
+"""
+c = 1 
+while c >= 1:
+    print
