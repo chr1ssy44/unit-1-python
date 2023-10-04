98d3c5e4861d4dad5ddaa099420f66351aab7352
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2023-10-04 10:24:12 -0400

    added a file

--- todo.py
+++ todo.py
@@ -1,20 +1,30 @@
 todos = ["do homework", "eat", "go to sleep"]
-i = 0
-print("Your current todos are: ")
-print(', '.join(todos))
-
-x = input("Would you like to add or remove a todo? ")
-
-while x == "add":
-    y = input("What is the new todo? ")
-    (todos.append(y))
+while True:
     print("Your current todos are: ")
     print(', '.join(todos))
-    print("Your current todos are: ")
-    break
-if x == "remove":
-    z = input("Which # todo would you like to remove? ")
-for i, todos in enumerate(todos, 1):
-    (todos.remove(z))
-    print(i, todos)
+    # I made a todo list of items and printed the current todos in the list. For the todos to show up without the brakets I used print(', '.join(todos)).
 
+    x = input("Would you like to add or remove a todo? ")
+
+    # I made an input to ask the user if they want to add or remove an item from the list.
+    if x == "add":
+        y = input("What is the new todo? ")
+        print("")
+        print("-------------------------------------------------")
+        (todos.append(y))
+        print("Your current todos are: ")
+        print("")
+        print(', '.join(todos))
+        print("")
+    # I made a while loop make x == add and to have to user add any other todo, then the code would add the item to the end of the list and print out the new list.
+
+    if x == "remove":
+        z = int(input("Which # todo would you like to remove? "))
+        z -= 1
+        del todos[z]
+        print("-------------------------------------------------")
+        print("Your current todos are: ")
+        print("")
+        print(', '.join(todos))
+        print("")
+    # I created an if statement for the user if they want to remove an item, then I made the input convert to an interger and for then to start at 1 and not 0 I subtracted 1 from the variable z.
