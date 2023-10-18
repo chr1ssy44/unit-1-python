b27c9dcb3f6258999319c03c34ac1d55110047d6
chr1ssy44 <cbbeckford2@brooklynsteamcenter.org>
2023-10-18 10:40:16 -0400

    added a file

--- return_of_the_function.py
+++ return_of_the_function.py
@@ -7,6 +7,10 @@ def number(a):
     return math.sqrt(a)
 y = number(25)
 
+assert number(25) == 5
+# The first assertion is True so nothing happened in the console.
+assert number(16) == 4
+# The second assertion is also True  because the squareroot of 16 is 4.
 print(y)
 #I import the math module and I created a function then return my function as math.sqrt(a) to find the square of the variable y which I set as 25.
 """
@@ -17,6 +21,13 @@ def area(c,d):
     return c*d
 x = area(15,4)
 
+assert area(15,4) == 60
+#The first assertion is True so nothing happened in the console.
+try:
+ assert area(9,9) == 80
+except:
+   print("9 times 9 is not equal to 80.")
+# For the second assertion i used try/expect to have to have an error but to say it gracefully instead of an AssertionError. So it would print to expect instead of posting an error.
 print(x)
 #I created a function and put two variable in the parameter then return it as c * d to find the area of the variable x which is 15 and 4.
 """
@@ -28,6 +39,10 @@ def temp(e):
     return e/9/5+32
 z = temp(21)
 
+assert temp(21) == 32.46666666666667
+# The first assertion is True so nothing happened in the console.
+assert temp(35) == 32.77777777777778
+# The second assertion is True so nothing happened in the console.
 print(z)
 
 """
